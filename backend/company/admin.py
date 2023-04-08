d7e273c95d25cc59918cdf2ff8e2d909b195a911
jeelpatel231 <jeelpatel231@gmail.com>
2023-04-08 13:38:51 +0530

    prototype notifying students with filtered departments

--- backend/company/admin.py
+++ backend/company/admin.py
@@ -18,21 +18,35 @@ class CompanyAdmin(ModelAdmin):
         "company_type",
     )
 
+
 class OpeningForDepartmentInline(admin.TabularInline):
     model = m.OpeningForDepartment
+
 
 @admin.register(m.CurrentOpening)
 class CurrentOpeningsAdmin(ModelAdmin):
     # TODO : generate message body and send emails to filtered out students
     @admin.action(description="Notify students")
     def notify_students_opening(self, request, queryset):
-        interested_students = Student.objects.filter(is_blocked=False)
-        # also filter through their departments
-        student_emails = [s.email for s in interested_students]
-        # make multiple message bodies from queryset and use send_mass_mail
         all_emails = []
-        for i in queryset:
-            print(i)
+        for opening in queryset:
+            # get all departments, that this opening is avaialable for
+            opening_departments = [
+                i.dept_id
+                for i in m.OpeningForDepartment.objects.filter(opening_id=opening.id)
+            ]
+            # filter out students, that are NOT selected, NOT blocked and are in the department
+            interested_students = [
+                s.email
+                for s in Student.objects.filter(
+                    is_blocked=False,
+                    is_selected=False,
+                    department__in=opening_departments,
+                )
+                # if s.email != ""
+            ]
+            # make multiple message bodies from queryset and use send_mass_mail
+            print(interested_students)
             # construct message body using title and description of the opening
             # all_emails.append(message_body)
         # send_mass_mail(all_emails, fail_silently=False)
